2ebab777c273c831a890f5bab38beda07381a280
Mohamad Sabha <mhd.sabha@hotmail.com>
2025-07-28 16:24:22 +0200

    create routs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Signin from "./pages/Signin";
+import Signup from "./pages/signup";
 export default function App() {
   return (
-    <h1 className="text-3xl text-red-950 font-bold underline">
-      this is my first bite application
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Sign-in" element={<Signin />} />
+        <Route path="/Sign-up" element={<Signup />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
