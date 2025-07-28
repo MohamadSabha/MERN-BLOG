2335f44f9c33f9c8cc6e31a57ad95eab10769019
Mohamad Sabha <mhd.sabha@hotmail.com>
2025-07-28 20:21:36 +0200

    database connection

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 // we changed the type in package.json to module in order to use import instead of require
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.DBCONNECTION)
+  .then(() => {
+    console.log("database is connected ");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server is running on post 3000222");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("server is running on post 3000");
 });
