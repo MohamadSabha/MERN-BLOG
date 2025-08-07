08186c1f66851fd9dd11a24c7dc9052731748b3a
Mohamad Sabha <mhd.sabha@hotmail.com>
2025-08-07 17:36:53 +0200

    get all posts and delete post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -10,5 +10,6 @@ const router = express.Router();
 
 router.post("/create", verifyToken, create);
 router.get("/getposts", getPosts);
+router.delete("/deletepost/:postId/:userId", verifyToken, deletePost);
 
 export default router;
